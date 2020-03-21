2306591bce294612225faae126139589e39ef3f5
Jack Lestrohan <jlestrohan@gmail.com>
2020-03-21 15:16:11 +0100

    Fixed explanations

--- STM32_G474RE/Core/Inc/sensor_speed_service.h
+++ STM32_G474RE/Core/Inc/sensor_speed_service.h
@@ -6,10 +6,10 @@
  *
  *      CONNECT TO A0 SENSOR SIDE, DO NOT USE D0!!
  *
- *      SENSOR 1 (TOP RIGHT looking up) to PC10
- *      SENSOR 2 (BTM RIGHT looking up) to PC12
- *      SENSOR 3 (BTM LEFT  looking up) to PC8
- *      SENSOR 4 (TOP LEFT  looking up) to PC9
+ *      SENSOR 1 (TOP RIGHT looking up) to PC9
+ *      SENSOR 2 (BTM RIGHT looking up) to PC10
+ *      SENSOR 3 (BTM LEFT  looking up) to PC11
+ *      SENSOR 4 (TOP LEFT  looking up) to PC12
  *
  */
 
