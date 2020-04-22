0336cfb657618bac944bac552394877b5159beef
Jack Lestrohan <jlestrohan@gmail.com>
2020-04-22 02:28:49 +0200

    Improved serial reception

--- Alinea-ESP32/src/main.cpp
+++ Alinea-ESP32/src/main.cpp
@@ -2,17 +2,23 @@
  * @ Author: Jack Lestrohan
  * @ Create Time: 2020-04-20 16:29:58
  * @ Modified by: Jack Lestrohan
- * @ Modified time: 2020-04-21 23:07:35
+ * @ Modified time: 2020-04-22 02:26:27
  * @ Description:
  *******************************************************************************************/
+
+// check https://github.com/muratdemirtas/ESP8266-UART-RX-INTERRUPT/blob/master/main.ino
+// check https://github.com/maxgerhardt/pio-stm32-with-esp8266-dht11
+// https://github.com/krzychb/esp-just-slip
+// https://github.com/martin-ger/esp_slip_router
+// https://forum.arduino.cc/index.php?topic=576983.0
 
 #include <Arduino.h>
 #include "ota.h"
 
 RemoteDebug Debug;
 
-uint32_t mLastTime = 0;
-uint32_t mTimeSeconds = 0;
+char uartBuffer[50];
+int uartBufferIndex = 0;
 
 /**
  * @brief  Main setup
@@ -25,7 +31,7 @@ void setup()
 
   setupOTA();
   debugI("Ready.");
-  	system_print_meminfo();
+  system_print_meminfo();
 
   Serial.println("Ready UART");
 }
@@ -38,33 +44,18 @@ void setup()
 void loop()
 {
   otaLoop();
-    
-  if ((millis() - mLastTime) >= 1000)
-  {
-    // Time
-    mLastTime = millis();
-    mTimeSeconds++;
 
-    // Debug the time (verbose level)
-    debugV("* Time: %u seconds (VERBOSE)", mTimeSeconds);
-    if (mTimeSeconds % 5 == 0)
-    { // Each 5 seconds
-      // Debug levels
-      debugV("* This is a message of debug level VERBOSE");
-      debugD("* This is a message of debug level DEBUG");
-      debugI("* This is a message of debug level INFO");
-      debugW("* This is a message of debug level WARNING");
-      debugE("* This is a message of debug level ERROR");
+  if (Serial.available())
+  {
+    char ch = (char)Serial.read();
+    if (ch == '\n') // is this the terminating carriage return
+    {
+      uartBuffer[uartBufferIndex] = 0; // terminate the string with a 0
+      uartBufferIndex = 0;         // reset the index ready for another string
+      // do something with the string
+      debugI("%s", uartBuffer);
     }
+    else
+      uartBuffer[uartBufferIndex++] = ch; // add the character into the buffer
   }
-
- if(Serial.available()>0)
-  {
-    byte x = Serial.read();
-    debugI("%c",x);
-    
-  }
-  
-  
 }
-
