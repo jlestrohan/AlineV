621344a6f07c59a80a1fbb88d3a7c82b984304b3
Jack Lestrohan <jlestrohan@gmail.com>
2020-04-23 12:26:40 +0200

    Moved a few constants

--- Alinea-ESP32/include/autoconnect_service.h
+++ Alinea-ESP32/include/autoconnect_service.h
@@ -2,14 +2,18 @@
  * @ Author: Jack Lestrohan
  * @ Create Time: 2020-04-22 22:13:03
  * @ Modified by: Jack Lestrohan
- * @ Modified time: 2020-04-22 22:15:31
+ * @ Modified time: 2020-04-23 12:16:26
  * @ Description:
  *******************************************************************************************/
 
 #ifndef _IC_AUTOCONNECT_SERVICE_H
 #define _IC_AUTOCONNECT_SERVICE_H
 
-void setupAutoConnect();
+// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
+const char thingName[] = "Alinea-ESP32";
+// -- Initial password to connect to the Thing, when it creates an own Access Point.
+const char wifiInitialApPassword[] = "73727170";
 
+void setupAutoConnect();
 
 #endif /* _IC_AUTOCONNECT_SERVICE_H */
