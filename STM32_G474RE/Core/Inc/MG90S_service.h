e619b43208117ace0b436c9a50071491a08831d5
Jack Lestrohan <jlestrohan@gmail.com>
2020-04-25 18:07:36 +0200

    Added HC-SR04 servo control skeleton files

---
+++ STM32_G474RE/Core/Inc/MG90S_service.h
@@ -0,0 +1,14 @@
+/*******************************************************************
+ * MG90S_service.h
+ *
+ *  Created on: 23 avr. 2020
+ *      Author: aez03
+ *
+ *******************************************************************/
+
+#ifndef INC_MG90S_SERVICE_H_
+#define INC_MG90S_SERVICE_H_
+
+
+
+#endif /* INC_MG90S_SERVICE_H_ */
