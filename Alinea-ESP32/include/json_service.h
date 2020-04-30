79abc747945c08c73b37f4fc8f1734bbcb812b61
Jack Lestrohan <jlestrohan@gmail.com>
2020-04-30 18:10:55 +0200

    Added JSON skeleton files

---
+++ Alinea-ESP32/include/json_service.h
@@ -0,0 +1,27 @@
+/*******************************************************************************************
+ * @ Author: Jack Lestrohan
+ * @ Create Time: 2020-04-30 00:12:26
+ * @ Modified by: Jack Lestrohan
+ * @ Modified time: 2020-04-30 16:30:37
+ * @ Description:
+ *******************************************************************************************/
+
+#ifndef _IC_JSON_SERVICE_H
+#define _IC_JSON_SERVICE_H
+
+typedef enum
+{
+
+}
+
+typedef struct
+{
+    uint8_t
+}
+
+// todo: check json xchange between stm32 and ESP32
+// use unions inside struct
+// https: //stackoverflow.com/questions/16623226/how-to-check-what-type-is-currently-used-in-union
+// https://arduinojson.org/v5/example/parser/
+
+#endif /* _IC_JSON_SERVICE_H */
