6b064272c1e15802f5e99231a5fc9a69da642724
Jack Lestrohan <jlestrohan@gmail.com>
2020-05-20 18:02:28 +0200

    Add MQTT AWS working connexion

--- Alinea-ESP32/include/autoconnect_service.h
+++ Alinea-ESP32/include/autoconnect_service.h
@@ -2,19 +2,17 @@
  * @ Author: Jack Lestrohan
  * @ Create Time: 2020-04-22 22:13:03
  * @ Modified by: Jack Lestrohan
- * @ Modified time: 2020-05-19 23:46:14
+ * @ Modified time: 2020-05-20 17:54:24
  * @ Description:
  *******************************************************************************************/
 
 #ifndef _IC_AUTOCONNECT_SERVICE_H
 #define _IC_AUTOCONNECT_SERVICE_H
 
+#include "FreeRTOS.h"
 #include "configuration_esp32.h"
 #include <stdint.h>
 #include "configuration_esp32.h"
-#include <WiFi.h>
-#include <WebServer.h>
-#include <AutoConnect.h>
 
 // -- Initial password to connect to the Thing, when it creates an own Access Point.
 const char wifiInitialApPassword[] = "73727170";
--- Alinea-ESP32/src/AWS_service.cpp
+++ Alinea-ESP32/src/AWS_service.cpp
@@ -2,23 +2,29 @@
  * @ Author: Jack Lestrohan
  * @ Create Time: 2020-05-18 09:28:57
  * @ Modified by: Jack Lestrohan
- * @ Modified time: 2020-05-20 08:36:02
+ * @ Modified time: 2020-05-20 18:00:14
  * @ Description:
  *******************************************************************************************/
 
-#include "FreeRTOS.h"
+#include "configuration_esp32.h"
 #include "aws_service.h"
 #include "remoteDebug_service.h"
 #include <stdio.h>
 #include "autoconnect_service.h"
-
-/* because ou json messages are quite small we can keep them in the stack to speed up the process */
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include "AWS_Certificate.h"
 
 static xTaskHandle xAWS_Send_Task_hnd = NULL;
 static xTaskHandle xAWS_Receive_Task_hnd = NULL;
+static xTaskHandle xAWS_Connect_Task_hnd = NULL;
 
-const char *awsEndpoint = "a2im1z2thfpkge-ats.iot.eu-west-3.amazonaws.com";
-// https://hieromon.github.io/AutoConnect/howtoembed.html#used-with-mqtt-as-a-client-application
+/* functions definitions */
+void connectToAWS();
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
 /* -------------------------------- AWS SEND -------------------------------- */
 /**
  * @brief  AWS SEND task. 
@@ -30,6 +36,7 @@ const char *awsEndpoint = "a2im1z2thfpkge-ats.iot.eu-west-3.amazonaws.com";
  */
 static void vAWSSendTaskCode(void *vParameter)
 {
+
     for (;;)
     {
 
@@ -61,25 +68,35 @@ static void vAWSReceiveTaskCode(void *vParameter)
     vTaskDelete(NULL);
 }
 
+/* ---------------------------- MQTT CONNECT TASK --------------------------- */
+void vAWSConnectTaskCode(void *vParameter)
+{
+    EventBits_t uxBits;
+
+    for (;;)
+    {
+
+        vTaskDelay(1000);
+    }
+    vTaskDelete(NULL);
+}
+
 /* --------------------------- MAIN INITIALIZATION -------------------------- */
 /**
  * @brief  Main Initialization routine
  * @note   
  * @retval 
  */
-uint8_t uSetupAwsService()
+uint8_t
+uSetupAwsService()
 {
     xQueueAWS_Send = xQueueCreate(3, sizeof(aws_rdy_data_t));
     if (xQueueAWS_Send == NULL)
     {
-        debugE("error creating the xQueueAWS_Send queue\n\r");
-        Serial.println("error creating the xQueueAWS_Send queue\n\r");
+        DEBUG_SERIAL("error creating the xQueueAWS_Send queue");
 
         return EXIT_FAILURE;
     }
-
-    /* creating PubSubClient handler */
-    //PubSubClient client(
 
     /* create tasks */
     /* let's create the parser task first */
@@ -94,6 +111,7 @@ uint8_t uSetupAwsService()
     if (xAWS_Send_Task_hnd == NULL)
     {
         debugE("Error creating serial parser task!");
+        Serial.println("Error creating serial parser task!");
         return EXIT_FAILURE;
     }
 
@@ -114,5 +132,57 @@ uint8_t uSetupAwsService()
         return EXIT_FAILURE;
     }
 
+    /* let's create the parser task first */
+    xTaskCreate(
+        vAWSConnectTaskCode,     /* Task function. */
+        "vAWSConnectTaskCode",   /* String with name of task. */
+        10000,                   /* Stack size in words. */
+        NULL,                    /* Parameter passed as input of the task */
+        12,                      /* Priority of the task. */
+        &xAWS_Connect_Task_hnd); /* Task handle. */
+
+    if (xAWS_Connect_Task_hnd == NULL)
+    {
+        debugE("Error creating xAWS_Connect_Task_hnd task!");
+        /* cannot create task, remove all created stuff and exit failure */
+        vQueueDelete(xQueueAWS_Send);
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
+}
+
+void connectToAWS()
+{
+    // Configure WiFiClientSecure to use the AWS certificates we generated
+    net.setCACert(AWS_CERT_CA);
+    net.setCertificate(AWS_CERT_CRT);
+    net.setPrivateKey(AWS_CERT_PRIVATE);
+
+    // Connect to the MQTT broker on the AWS endpoint we defined earlier
+    client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+    // Try to connect to AWS and count how many times we retried.
+    int retries = 0;
+    DEBUG_SERIAL("Connecting to AWS IOT");
+
+    while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES)
+    {
+        Serial.print(".");
+        delay(100);
+        retries++;
+    }
+
+    // Make sure that we did indeed successfully connect to the MQTT broker
+    // If not we just end the function and wait for the next loop.
+    if (!client.connected())
+    {
+        Serial.println(" Timeout!");
+        debugE("MQTT Timeout!...");
+        return;
+    }
+
+    // If we land here, we have successfully connected to AWS!
+    // And we can subscribe to topics and send messages.
+    DEBUG_SERIAL("MQTT Connected!");
 }
